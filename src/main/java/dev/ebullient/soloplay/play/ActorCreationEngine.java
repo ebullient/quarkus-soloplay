dc8574bc3e27af9c921a105fb04f9d0378bf0e2c
Erin Schnabel <ebullientworks@gmail.com>
2026-01-19 10:35:20 -0500

    🐛 Fix accidental indent of character info

--- src/main/java/dev/ebullient/soloplay/play/ActorCreationEngine.java
+++ src/main/java/dev/ebullient/soloplay/play/ActorCreationEngine.java
@@ -76,10 +76,10 @@ public class ActorCreationEngine {
             game.setGamePhase(game.getGamePhase().next());
             cleanupDraft(game);
             return GameResponse.reply("""
-                        Created your character: **%s** (%s, level %s).
+                    Created your character: **%s** (%s, level %s).
 
-                        Use `/newcharacter` to create an additional character, or `/start` to start or resume your game.
-                    """.formatted(actor.getName(), actor.getActorClass(), actor.getLevel()));
+                    Use `/newcharacter` to create an additional character, or `/start` to start or resume your game.
+                    """.stripIndent().formatted(actor.getName(), actor.getActorClass(), actor.getLevel()));
         }
 
         emitter.assistantDelta("The GM is thinking…\n");
